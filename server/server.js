830098643aa8a8341d108c7388b672cbe26faa47
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-01-01 19:54:09 +0000

    Integrated cookie-session and keygrip middleware to handle session cookies

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 // Set server address, port and required packages.
 const PORT = parseInt(process.env.PORT || "5000");
 const SERVER = "localhost";
+const cookieSession = require("cookie-session");
 const express = require("express");
 const routes = require("./routes/routes");
 const authRoutes = require("./routes/authRoutes");
 require("./authentication/passportConfig");
 const app = express();
+const Keygrip = require("keygrip");
+
+// Configure session cookies with 24hr expiration and random keys.
+app.use(
+  cookieSession({
+    name: "session",
+    keys: new Keygrip(["key1", "key2"], "SHA384"), // Keygrip instance used to generate keys.
+    maxAge: 24 * 60 * 60 * 100, // 24 hours
+  })
+);
 
 routes(app);
 
