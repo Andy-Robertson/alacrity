5accb44e16bc5e50496307fe364293c90f2c1076
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-01-01 20:14:39 +0000

    Configure app instance with cors middleware and set /auth redirect to authRoutes

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true,
+  })
+);
+app.use("/auth", authRoutes);
 
 routes(app);
 
