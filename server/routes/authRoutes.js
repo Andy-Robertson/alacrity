3501598b0ecac572ffa8801d5453297b6fcfc8d1
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-01-01 19:38:22 +0000

    Added google scope and google callback redirection routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,7 +3,7 @@ const passport = require("passport");
 const CLIENT_URL = "http://localhost:3000/";
 
 // Handle successful login (user authenticated).
-router.get("/login/success", (req, res) => {
+authRouter.get("/login/success", (req, res) => {
   if (req.user) {
     res.status(200).json({
       success: true,
@@ -15,7 +15,7 @@ router.get("/login/success", (req, res) => {
 });
 
 // Handle failed login (user not authenticated).
-router.get("/login/failed", (req, res) => {
+authRouter.get("/login/failed", (req, res) => {
   res.status(401).json({
     success: false,
     message: "failed to authenticate",
@@ -23,9 +23,26 @@ router.get("/login/failed", (req, res) => {
 });
 
 // Handle logout
-router.get("/logout", (req, res) => {
+authRouter.get("/logout", (req, res) => {
   req.logout();
   res.redirect(CLIENT_URL);
 });
+
+//     ---------- GOOGLE AUTHENTICATION ----------     //
+
+// Set request scope, user profile and email returned on successful auth.
+authRouter.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+// Set redirect routes upon authentication success or failure.
+authRouter.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: CLIENT_URL,
+    failureRedirect: "/login/failed",
+  })
+);
 
 module.exports = authRouter;
