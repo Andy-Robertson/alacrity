f6b6cce38b7d979c8e3e53ef78df8180ea3a2b0e
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-01-01 19:20:54 +0000

    Added failed authentication route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,16 +2,24 @@ const authRouter = require("express").Router();
 const passport = require("passport");
 const CLIENT_URL = "http://localhost:3000/";
 
-// Handle successful login (user authenticated)
+// Handle successful login (user authenticated).
 router.get("/login/success", (req, res) => {
   if (req.user) {
     res.status(200).json({
       success: true,
-      message: "successful",
+      message: "successful authentication",
       user: req.user,
       cookies: req.cookies,
     });
   }
+});
+
+// Handle failed login (user not authenticated).
+router.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "failed to authenticate",
+  });
 });
 
 module.exports = authRouter;
