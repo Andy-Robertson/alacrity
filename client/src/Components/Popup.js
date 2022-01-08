cac44343bfc2bf4517ece4f71e8ab73ecb08aa2b
Jalal <ahmedjalalsd@gmail.com>
2022-01-08 18:34:38 +0000

    add form to popup

--- client/src/Components/Popup.js
+++ client/src/Components/Popup.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import Toggle from "./Toggle";
 
 const Popup = props => {
+    const [toggled, setToggled] = useState(false);
     return (
         <div className="popup-box">
             <div className="box">
                 <span className="close-icon" onClick={() => props.close(false)}>x</span>
-                {props.content}
+                <form className="form">
+                    <div>
+                        <input
+                            type="text"
+                            name="task-subject"
+                            placeholder="Task Subject"
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            name="description"
+                            placeholder="Description"
+                        />
+                    </div>
+                    <h4>Sub Tasks</h4>
+                    <Toggle handleCheck={(evt) => setToggled(evt.target.checked)} />
+                    {/* <p> the button is {toggled ? "on" : "off"}</p> */}
+                    {toggled ? <div>
+                        <input
+                            type="text"
+                            name="sub-task"
+                            placeholder="Sub Task"
+                        />
+                    </div> : null}
+                    <div>
+                        <input
+                            type="text"
+                            name="reward"
+                            placeholder="Reward"
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            name="resources"
+                            placeholder="resources"
+                        />
+                    </div>
+                </form>
             </div>
+
         </div>
     );
 };
