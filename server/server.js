24d19cfd110ea04575dadc74b8990754460304f5
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2022-01-08 19:44:10 +0000

    clean back end

--- server/server.js
+++ server/server.js
@@ -1,45 +1,8 @@
-// Set server address, port and required packages.
 const PORT = parseInt(process.env.PORT || "5000");
-const SERVER = "localhost";
-const Keygrip = require("keygrip");
-const passport = require("passport");
-const cors = require("cors");
-const cookieSession = require("cookie-session");
 const express = require("express");
-const routes = require("./routes/routes");
-const authRoutes = require("./routes/authRoutes");
-require("./authentication/passportConfig");
+
 const app = express();
 
-// Configure session cookies with 24hr expiration and random keys.
-app.use(
-  cookieSession({
-    name: "session",
-    keys: new Keygrip(["key1", "key2"], "SHA384"), // Keygrip instance used to generate keys.
-    maxAge: 24 * 60 * 60 * 100, // 24 hours
-  })
+app.listen(PORT, () =>
+  console.log(`Alacrity server now gravitating on http://localhost:${PORT}`)
 );
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: "GET, POST, PUT, DELETE",
-    credentials: true,
-  })
-);
-app.use("/auth", authRoutes);
-
-routes(app);
-
-// Start alacrity server
-const server = app.listen(PORT, SERVER, (err) => {
-  err
-    ? console.log(`Error: ${err}`)
-    : console.log(
-        `Alacrity server now gravitating on ${server.address().address}:${
-          server.address().port
-        }`
-      );
-});
