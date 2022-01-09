4c3b9524962db0241b598f4a0512c401d7117af5
Calum Scott <calum.scott@and.digital>
2022-01-09 14:47:55 +0000

    updating server config for deployment

--- server/server.js
+++ server/server.js
@@ -1,35 +1,35 @@
 // Set server address, port and required packages.
-const PORT = parseInt(process.env.PORT || "5000");
-const SERVER = "localhost";
-const Keygrip = require("keygrip");
-const passport = require("passport");
-const cors = require("cors");
-const cookieSession = require("cookie-session");
-const express = require("express");
-const routes = require("./routes/routes");
-const authRoutes = require("./routes/authRoutes");
-require("./authentication/passportConfig");
+const PORT = parseInt(process.env.PORT || '5000');
+const SERVER = '0.0.0.0';
+const Keygrip = require('keygrip');
+const passport = require('passport');
+const cors = require('cors');
+const cookieSession = require('cookie-session');
+const express = require('express');
+const routes = require('./routes/routes');
+const authRoutes = require('./routes/authRoutes');
+require('./authentication/passportConfig');
 const app = express();
 
 // Configure session cookies with 24hr expiration and random keys.
 app.use(
   cookieSession({
-    name: "session",
-    keys: new Keygrip(["key1", "key2"], "SHA384"), // Keygrip instance used to generate keys.
+    name: 'session',
+    keys: new Keygrip(['key1', 'key2'], 'SHA384'), // Keygrip instance used to generate keys.
     maxAge: 24 * 60 * 60 * 100, // 24 hours
-  })
+  }),
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    methods: "GET, POST, PUT, DELETE",
+    origin: 'http://localhost:3000',
+    methods: 'GET, POST, PUT, DELETE',
     credentials: true,
-  })
+  }),
 );
-app.use("/auth", authRoutes);
+app.use('/auth', authRoutes);
 
 routes(app);
 
@@ -40,6 +40,6 @@ const server = app.listen(PORT, SERVER, (err) => {
     : console.log(
         `Alacrity server now gravitating on ${server.address().address}:${
           server.address().port
-        }`
+        }`,
       );
 });
