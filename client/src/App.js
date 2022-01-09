0c36023e378d5815af5a93eb5952d7a2592aa0ec
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-01-09 17:30:15 +0000

    Added fetch user promise and state variable to store returned object

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // Assets
 import "./Assets/styles/main.css";
 import "./Assets/styles/normalize.css";
@@ -7,9 +7,37 @@ import "./Assets/styles/style.css";
 import LeftSideBar from "./Layouts/LeftSideBar";
 import Middle from "./Layouts/Middle";
 import RightSideBar from "./Layouts/RightSideBar";
-import Login from "./Layouts/Login"
+import Login from "./Layouts/Login";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUser = () => {
+      fetch("http://localhost:5000/auth/login/success", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true,
+        },
+      })
+        .then((response) => {
+          console.log(response);
+          if (response.status === 200) return response.json();
+          throw new Error("authentication has failed");
+        })
+        .then((resObject) => {
+          setUser(resObject.user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getUser();
+  }, []);
+
   return (
     <main>
       <LeftSideBar />
