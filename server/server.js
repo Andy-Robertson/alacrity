ee29f769f6c6c01fa35ed0c00e8ccc3bdcf4d0f3
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-01-12 17:26:27 +0000

    Add missing require for pg

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const express = require("express");
 const routes = require("./routes/routes");
 const authRoutes = require("./routes/authRoutes");
 require("./authentication/passportConfig");
+require("./data/postgresConfig")
 const app = express();
 
 // Production / Development environment selection.
