176ddd3ea04103e659a9cd6e05929763309e64fd
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2022-01-19 21:11:36 +0000

    add delete button for subtask

--- client/src/Components/AddTask/AddSubTask.js
+++ client/src/Components/AddTask/AddSubTask.js
@@ -2,19 +2,27 @@ import React, { useState } from "react";
 
 function AddSubTask(props) {
   const [subTask, setSubTask] = useState("");
+  const [isDelete, setIsDelete] = useState(true);
   const change = (e) => {
     setSubTask(e.target.value);
     props.changeHandler(e, props.index); // send e (event) and index to the changeHandler function which is in the Form component.
   };
   return (
     <div key={props.index}>
-      <input
-        type="text"
-        name={"sub-task" + props.index}
-        placeholder="Sub Task"
-        value={subTask}
-        onChange={change}
-      />
+      {isDelete && (
+        <div>
+          <input
+            type="text"
+            name={"sub-task" + props.index}
+            placeholder="Sub Task"
+            value={subTask}
+            onChange={change}
+          />
+          <button onClick={() => setIsDelete(false)}>
+            <i className="fa fa-minus"></i>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
