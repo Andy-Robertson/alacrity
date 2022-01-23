cd7408bb787a35de5ca165cc26a791d761643da1
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-01-23 10:28:01 +0000

    Fix passport session  request access path

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -27,7 +27,7 @@ const router = (app) => {
 
   // Load user settings.
   app.get("/api/settings", (req, res) => {
-    const auth_id = req.passport.user;
+    const auth_id = req.session.passport.user;
 
     pool
       .query(FIND_USER_BY_ID, [auth_id])
@@ -46,7 +46,7 @@ const router = (app) => {
 
   // Update user settings.
   app.put("/api/settings", (req, res) => {
-    const auth_id = req.passport.user;
+    const auth_id = req.session.passport.user;
     const { pom_minutes, pom_seconds } = req.body;
 
     pool
