28f5d749a264e6f212c6806b228a3278b0ffd489
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-01-25 09:35:59 +0000

    Fix pomodoro timer not showing a default time when a user first creates an account

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -25,73 +25,72 @@ const router = (app) => {
     });
   });
 
-
   // Load tasks (users.auth_id).
   app.get("/api/tasks", (req, res) => {
     // console.log(req.session.passport.user);
-    const auth_id = req.session.passport.user; 
-  pool
-    .query("SELECT * FROM users WHERE auth_id = $1", [auth_id])
-    .then((result) => {
-      const user_id = result.rows[0].id;
-      // console.log(user_id)
-      pool
-        .query("SELECT * FROM task WHERE user_id = $1", [user_id])
-        .then((result) => {
-          // console.log(result.rows);
-          res.status(200).json(result.rows);
-        })
-        .catch((e) => console.error(e));
-    })
-    .catch((e) => console.error(e));
+    const auth_id = req.session.passport.user;
+    pool
+      .query("SELECT * FROM users WHERE auth_id = $1", [auth_id])
+      .then((result) => {
+        const user_id = result.rows[0].id;
+        // console.log(user_id)
+        pool
+          .query("SELECT * FROM task WHERE user_id = $1", [user_id])
+          .then((result) => {
+            // console.log(result.rows);
+            res.status(200).json(result.rows);
+          })
+          .catch((e) => console.error(e));
+      })
+      .catch((e) => console.error(e));
   });
 
   // Add user tasks (users.auth_id).
   app.post("/api/tasks", (req, res) => {
     // console.log(req.session.passport.user);
-const auth_id = req.session.passport.user;
-const task_subject = req.body.task_subject;
-const subject_description = req.body.subject_description;
-const reward = req.body.reward;
-const resources = req.body.resources;
-const by_time = req.body.by_time;
-const by_date = req.body.by_date;
-const sub_task_option = req.body.sub_task_option;
-const sub_tasks = req.body.sub_tasks;
-// console.log(req.body);
-pool
-  .query("SELECT * FROM users WHERE auth_id = $1", [auth_id])
-  .then((result) => {
-    const user_id = result.rows[0].id;
-    // console.log(user_id);
+    const auth_id = req.session.passport.user;
+    const task_subject = req.body.task_subject;
+    const subject_description = req.body.subject_description;
+    const reward = req.body.reward;
+    const resources = req.body.resources;
+    const by_time = req.body.by_time;
+    const by_date = req.body.by_date;
+    const sub_task_option = req.body.sub_task_option;
+    const sub_tasks = req.body.sub_tasks;
+    // console.log(req.body);
     pool
-      .query("SELECT * FROM task WHERE user_id = $1", [user_id])
-      .then(() => {
-        const query =
-          "INSERT INTO task (user_id, task_subject, subject_description, reward, resources, by_time, by_date, sub_task_option, sub_tasks) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
+      .query("SELECT * FROM users WHERE auth_id = $1", [auth_id])
+      .then((result) => {
+        const user_id = result.rows[0].id;
+        // console.log(user_id);
         pool
-          .query(query, [
-            user_id,
-            task_subject,
-            subject_description,
-            reward,
-            resources,
-            by_time,
-            by_date,
-            sub_task_option,
-            sub_tasks,
-          ])
+          .query("SELECT * FROM task WHERE user_id = $1", [user_id])
           .then(() => {
-            res.sendStatus(204);
+            const query =
+              "INSERT INTO task (user_id, task_subject, subject_description, reward, resources, by_time, by_date, sub_task_option, sub_tasks) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
+            pool
+              .query(query, [
+                user_id,
+                task_subject,
+                subject_description,
+                reward,
+                resources,
+                by_time,
+                by_date,
+                sub_task_option,
+                sub_tasks,
+              ])
+              .then(() => {
+                res.sendStatus(204);
+              })
+              .catch((e) => console.error(e));
           })
           .catch((e) => console.error(e));
       })
       .catch((e) => console.error(e));
-  })
-  .catch((e) => console.error(e));
   });
 
-  // Edit user tasks 
+  // Edit user tasks
   app.put("/api/tasks", (req, res) => {
     // console.log(req.session.passport.user);
     console.log(req.body);
@@ -106,22 +105,22 @@ pool
     const sub_tasks = req.body.sub_tasks;
     const query =
       "UPDATE task SET task_subject = $1, subject_description = $2, reward = $3, resources= $4, by_time = $5, by_date = $6, sub_task_option = $7, sub_tasks = $8 WHERE id = $9;";
-      pool
-        .query(query, [
-          task_subject,
-          subject_description,
-          reward,
-          resources,
-          by_time,
-          by_date,
-          sub_task_option,
-          sub_tasks,
-          subject_id,
-        ])
-        .then((result) => {
-          res.sendStatus(204);
-        })
-        .catch((e) => console.error(e));;
+    pool
+      .query(query, [
+        task_subject,
+        subject_description,
+        reward,
+        resources,
+        by_time,
+        by_date,
+        sub_task_option,
+        sub_tasks,
+        subject_id,
+      ])
+      .then((result) => {
+        res.sendStatus(204);
+      })
+      .catch((e) => console.error(e));
   });
 
   // Load user settings.
