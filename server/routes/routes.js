6f2f47a59fbc5542faa39c53294d1c85f8ab5a9a
Jalal <ahmedjalalsd@gmail.com>
2022-01-29 13:07:32 +0000

    revert all changes on route.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -148,68 +148,36 @@ const router = (app) => {
 
   // Edit user tasks
   app.put("/api/tasks", (req, res) => {
-    const auth_id = req.session.passport.user;
-    const {
-      id,
-      task_subject,
-      subject_description,
-      sub_task_option,
-      sub_tasks,
-      reward,
-      resources,
-      by_time,
-      by_date,
-    } = req.body;
-    // console.log(req.body);
 
+    const subject_id = req.body.id;
+    const task_subject = req.body.task_subject;
+    const subject_description = req.body.subject_description;
+    const reward = req.body.reward;
+    const resources = req.body.resources;
+    const by_time = req.body.by_time;
+    const by_date = req.body.by_date;
+    const sub_task_option = req.body.sub_task_option;
+    const sub_tasks = req.body.sub_tasks;
+    const query =
+      "UPDATE task SET task_subject = $1, subject_description = $2, reward = $3, resources= $4, by_time = $5, by_date = $6, sub_task_option = $7, sub_tasks = $8 WHERE id = $9;";
     pool
-      .query("SELECT * FROM users WHERE auth_id = $1", [auth_id])
+      .query(query, [
+        task_subject,
+        subject_description,
+        reward,
+        resources,
+        by_time,
+        by_date,
+        sub_task_option,
+        sub_tasks,
+        subject_id,
+      ])
       .then((result) => {
-        const user_id = result.rows[0].id;
-        
-        pool
-            const query =
-            'UPDATE task SET task_subject = $2, subject_description = $3, reward = $4, resources= $5, by_time = $6, by_date = $7, sub_task_option = $8, sub_tasks = $9 WHERE id = $10 and user_id = $1';
-            pool
-              .query(query, [
-                user_id,
-                task_subject,
-                subject_description,
-                reward,
-                resources,
-                by_time,
-                by_date,
-                sub_task_option,
-                sub_tasks,
-                id,
-              ])
-              .then((result) => {
-                // console.log(id);
-                res.sendStatus(204);
-              })
-              .catch((e) => console.error(e));
-          })
-          .catch((e) => console.error(e));
-
-    // const query =
-    //   "UPDATE task SET task_subject = $1, subject_description = $2, reward = $3, resources= $4, by_time = $5, by_date = $6, sub_task_option = $7, sub_tasks = $8 WHERE id = $9;";
-    // pool
-    //   .query(query, [
-    //     task_subject,
-    //     subject_description,
-    //     reward,
-    //     resources,
-    //     by_time,
-    //     by_date,
-    //     sub_task_option,
-    //     sub_tasks,
-    //     subject_id,
-    //   ])
-    //   .then((result) => {
-    //     res.sendStatus(201);
-    //   })
-    //   .catch((e) => console.error(e));
+        res.sendStatus(201);
+      })
+      .catch((e) => console.error(e));
   });
+
 
   app.put("/api/tasks/archived", (req, res) => {
     const { task_id, task_archived } = req.body;
@@ -220,13 +188,13 @@ const router = (app) => {
       .query(query, [task_archived, task_id])
       .then((result) => {
         result.rows ?
-           res.status(200).json({
-              Result: "Success",
-              message: "Request complete: task archive status updated",
-            })
+          res.status(200).json({
+            Result: "Success",
+            message: "Request complete: task archive status updated",
+          })
           : res
-              .status(500)
-              .json({ Result: "Failure", message: "Request not complete" });
+            .status(500)
+            .json({ Result: "Failure", message: "Request not complete" });
       })
       .catch((e) => console.error(e));
   });
@@ -240,12 +208,12 @@ const router = (app) => {
       .then((result) => {
         result.rows
           ? res.status(200).json({
-              pom_minutes: result.rows[0].pom_minutes,
-              pom_seconds: result.rows[0].pom_seconds,
-            })
+            pom_minutes: result.rows[0].pom_minutes,
+            pom_seconds: result.rows[0].pom_seconds,
+          })
           : res
-              .status(500)
-              .json({ Result: "Failure", message: "Request not complete" });
+            .status(500)
+            .json({ Result: "Failure", message: "Request not complete" });
       })
       .catch((e) => console.error(e));
   });
@@ -260,22 +228,22 @@ const router = (app) => {
       .then((result) => {
         result.rowCount > 0
           ? res
-              .status(200)
-              .json({ Result: "Success", message: "Settings updated" })
+            .status(200)
+            .json({ Result: "Success", message: "Settings updated" })
           : res
-              .status(500)
-              .json({ Result: "Failure", message: "Request not complete" });
+            .status(500)
+            .json({ Result: "Failure", message: "Request not complete" });
       })
       .catch((e) => console.error(e));
   });
-  
+
   // Error handling
   app.use((req, res) => {
     res.status(404).json({
       message: "Route Not Found",
     });
   });
-  
+
   app.use((err, req, res) => {
     res.status(err.status || 500).json({
       message: err.message,
