7d4a8196901511d0c097e8644e6f794f83522ed0
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-02-06 11:07:30 +0000

    Add a pomodoro and task context

--- client/src/Layouts/Middle.js
+++ client/src/Layouts/Middle.js
@@ -1,18 +1,24 @@
-import React from "react";
-// import FocusTaskView from "../Components/FocusMode/FocusTaskView";
+import React, { useContext } from "react";
+import FocusTaskView from "../Components/FocusMode/FocusTaskView";
 import TaskListView from "../Components/FocusMode/TaskListView";
+import { TaskAndPomContext } from "../Contexts/TaskAndPomContext";
 
 const Middle = ({ user, taskData, submitComplete }) => {
+  const { isTaskFocused } = useContext(TaskAndPomContext);
 
   return (
     <section className="wrapper">
-      {/* <FocusTaskView /> */}
-
-      <TaskListView
-        user={user}
-        taskData={taskData}
-        submitComplete={submitComplete}
-      />
+      <TaskAndPomContext.Provider value={{ isTaskFocused }}>
+        {isTaskFocused ? (
+          <FocusTaskView taskData={taskData} submitComplete={submitComplete} />
+        ) : (
+          <TaskListView
+            user={user}
+            taskData={taskData}
+            submitComplete={submitComplete}
+          />
+        )}
+      </TaskAndPomContext.Provider>
     </section>
   );
 };
