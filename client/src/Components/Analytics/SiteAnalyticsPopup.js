ec273455f0f794136d68455e86fcd9117977996c
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2022-02-09 00:43:27 +0000

    add site analytics pop up

---
+++ client/src/Components/Analytics/SiteAnalyticsPopup.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function SiteAnalyticsPopup(props) {
+ const handlePropagation = (e) => {
+   e.stopPropagation();
+ };
+  return (
+  <div
+      className="popup-box animate__animated animate__fadeIn"
+      onClick={() => props.close(false)}
+    >
+      <div
+        className="box animate__animated animate__fadeInUpBig"
+        onClick={(e) => handlePropagation(e)}
+      >
+        <div className="buttons">
+          <button className="btn cancel" onClick={() => props.close(false)}>
+            <span>Cancel</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SiteAnalyticsPopup;
--- client/src/Layouts/LeftSideBar.js
+++ client/src/Layouts/LeftSideBar.js
@@ -7,10 +7,12 @@ import DashBoardImg from "../Assets/img/dashboard-24.png";
 import PieChartImg from "../Assets/img/pie-chart-24.png";
 import SettingsImg from "../Assets/img/settings-24.png";
 import SiteSettingsPopup from "../Components/Settings/SiteSettingsPopup";
+import SiteAnalyticsPopup from "../Components/Analytics/SiteAnalyticsPopup";
 
 const LeftSideBar = ({ user }) => {
   const [settingsIsOpen, setSettingsIsOpen] = useState(false);
-    const { isTaskFocused } = useContext(GlobalContext);
+  const [analyticsIsOpen, setAnalyticsIsOpen] = useState(false);
+  const { isTaskFocused } = useContext(GlobalContext);
   return (
     <aside className="left-sidebar">
       {user && !isTaskFocused && <img src={Logo} alt="logo"></img>}
@@ -28,76 +30,82 @@ const LeftSideBar = ({ user }) => {
 
       {user && !isTaskFocused && (
         <>
-        <div id="menuBigScreen" className="items animate__animated animate__fadeInLeftBig">
-          <div>
-            <a href="#left-sidebar">
-              <img src={DashBoardImg} alt="dashboard logo"></img>
-              <span>Dashboard</span>
-            </a>
-          </div>
-          <div>
-            <a href="#">
-              <img src={PieChartImg} alt="Analytics logo"></img>
-              <span>Analytics</span>
-            </a>
-          </div>
           <div
-            onClick={() => {
-              setSettingsIsOpen(true);
-            }}
-            className="settings-btn"
+            id="menuBigScreen"
+            className="items animate__animated animate__fadeInLeftBig"
           >
-            <img src={SettingsImg} alt="settings logo"></img>
-            <span>Settings</span>
-          </div>
-        </div>
-        {/* for smaller screen */}
-        <nav role="navigation">
-          <div id="menuToggle">
-            {/* A fake / hidden checkbox is used as click reciever,
-    so you can use the :checked selector on it. */}
-            <input type="checkbox" />
-
-            {/* Some spans to act as a hamburger. */}
-            <span></span>
-            <span></span>
-            <span></span>
-
-            {/* Too bad the menu has to be inside of the button
-    but hey, it's pure CSS magic.*/}
-            <ul id="menu">
+            <div>
               <a href="#left-sidebar">
-                <li className="menu-li">
-                  <img src={DashBoardImg} alt="dashboard logo"></img>
-                  <div className="menu-div">Dashboard</div>
-                </li>
+                <img src={DashBoardImg} alt="dashboard logo"></img>
+                <span>dashboard</span>
               </a>
-              <a href="#">
-                <li className="menu-li">
-                  <img src={PieChartImg} alt="Analytics logo"></img>
-                  <div className="menu-div">Analytics</div>
-                </li>
-              </a>
+            </div>
+            <div
+              onClick={() => setAnalyticsIsOpen(true)}
+              className="analytics-btn"
+            >
               <a>
-                <li className="menu-li">
-                  <div
-                    onClick={() => {
-                      setSettingsIsOpen(true);
-                    }}
-                    className="settings-btn"
-                  >
-                    <img src={SettingsImg} alt="settings logo"></img>
-                    <div className="menu-div">Settings</div>
-                  </div>
-                </li>
+                <img src={PieChartImg} alt="Analytics logo"></img>
+                <span>Analytics</span>
               </a>
-
-            </ul>
+            </div>
+            <div
+              onClick={() => {
+                setSettingsIsOpen(true);
+              }}
+              className="settings-btn"
+            >
+              <img src={SettingsImg} alt="settings logo"></img>
+              <span>Settings</span>
+            </div>
           </div>
-        </nav>
+          {/* for smaller screen */}
+          <nav role="navigation">
+            <div id="menuToggle">
+              {/* A fake / hidden checkbox is used as click reciever,
+    so you can use the :checked selector on it. */}
+              <input type="checkbox" />
+
+              {/* Some spans to act as a hamburger. */}
+              <span></span>
+              <span></span>
+              <span></span>
+
+              {/* Too bad the menu has to be inside of the button
+    but hey, it's pure CSS magic.*/}
+              <ul id="menu">
+                <a href="#left-sidebar">
+                  <li className="menu-li">
+                    <img src={DashBoardImg} alt="dashboard logo"></img>
+                    <div className="menu-div">Dashboard</div>
+                  </li>
+                </a>
+                <a href="#" onClick={() => setAnalyticsIsOpen(true)}>
+                  <li className="menu-li">
+                    <img src={PieChartImg} alt="Analytics logo"></img>
+                    <div className="menu-div">Analytics</div>
+                  </li>
+                </a>
+                <a>
+                  <li className="menu-li">
+                    <div
+                      onClick={() => {
+                        setSettingsIsOpen(true);
+                      }}
+                      className="settings-btn"
+                    >
+                      <img src={SettingsImg} alt="settings logo"></img>
+                      <div className="menu-div">Settings</div>
+                    </div>
+                  </li>
+                </a>
+              </ul>
+            </div>
+          </nav>
         </>
       )}
       {settingsIsOpen && <SiteSettingsPopup close={setSettingsIsOpen} />}
+      {analyticsIsOpen && <SiteAnalyticsPopup close={setAnalyticsIsOpen} />}
     </aside>
   );
 };
