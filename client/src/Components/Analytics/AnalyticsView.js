bdb9214b5a1555d25c699c71da3fb1c7678d35f5
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2022-02-12 09:13:33 +0000

    add the charts to analytic mode THANKS TO ANDY

--- client/src/Components/Analytics/AnalyticsView.js
+++ client/src/Components/Analytics/AnalyticsView.js
@@ -1,12 +1,49 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import MonthlyLineChart from "./MonthlyLineChart";
+import TotalDouhnutChart from "./TotalDouhnutChart";
+import WeeklyLineChart from "./WeeklyLineChart";
 import { GlobalContext } from "../../Contexts/GlobalContext";
 
 const AnalyticsView = () => {
   const { setIsAnalyticsFocused } = useContext(GlobalContext);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("/api/tasks/analytics")
+      .then((response) => response.json())
+      .then((analyticsData) => {
+        setData(analyticsData);
+      });
+  }, []);
+  const handlePropagation = (e) => {
+    e.stopPropagation();
+  };
 
   return (
     <>
-      <h1>Enjoy playing with this abadi, and get some sleep!</h1>
+      <div
+        className="box-analytics animate__animated animate__fadeInUpBig"
+        onClick={(e) => handlePropagation(e)}
+      >
+        <div>
+          {data.length > 0 ? (
+            <div>
+              <h2 className="title-analytics">
+                {" "}
+                Analytics For Completed Tasks VS Uncompleted Tasks
+              </h2>
+              <div className="chart">
+                <div className="line-chart-container">
+                  <WeeklyLineChart data={data} />
+                  <MonthlyLineChart data={data} />
+                </div>
+                <TotalDouhnutChart data={data} />
+              </div>
+            </div>
+          ) : (
+            <h1>There is no data to analyse</h1>
+          )}
+        </div>
+        </div>
 
       <div className="buttons">
         <button
